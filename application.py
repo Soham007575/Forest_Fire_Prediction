54b43120f710b7ff4dc3675abda3c96a69aefcc5
Soham Antre <sohamantre007@gmail.com>
2026-03-13 23:49:30 +0530

    Full code complete

--- application.py
+++ application.py
@@ -10,6 +10,8 @@ app = application
 ridge_model = pickle.load(open('models/ridge.pkl','rb'))
 Standard_Scaler = pickle.load(open('models/scaler.pkl','rb'))
 
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -17,9 +19,24 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
-        pass
+        Temperature=float(request.form.get('Temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        Region=float(request.form.get('Region'))
+        
+        new_data_scaler=Standard_Scaler.transfrom([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaler)
+
+        return render_template('home.html',resukts=result[0])
+        
     else:
         return render_template('home.html')
+
 
 
 if __name__=="__main__":
